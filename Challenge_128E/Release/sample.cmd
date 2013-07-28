b613dd1afd3c19c18da364fa2551fe79582e7ae2
powder96 <powder96@example.org>
2013-07-28 04:40:18 -0400

    Challenge 128E

---
+++ Challenge_128E/Release/sample.cmd
@@ -0,0 +1,18 @@
+@echo off
+echo Challenge 128E: Sample
+echo ======================
+echo.
+echo Input
+echo -----
+type sample.dat
+echo.
+echo.
+echo Expected output
+echo ---------------
+type sample.txt
+echo.
+echo Actual output
+echo -------------
+Challenge_128E < sample.dat
+echo.
+pause
